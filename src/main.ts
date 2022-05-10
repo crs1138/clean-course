91606664f9dff6a3edafa864d65f99b181f74921
Jan Honza Pozivil <jpozivil@leadtech.com>
2022-05-10 07:28:46 +0200

    Code Smell
    Details:
    ---
    * singleton
    * bajo acoplamiento, alta cohesion

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
-import './clean-code/07-tarea'
+import "./style.css";
+import "./code-smell/02-low-coupling";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
+`;
