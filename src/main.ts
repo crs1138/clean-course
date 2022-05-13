e5bf0ee015d987f6ecdcd66e83210a9390538566
Jan Pozivil <crs1138@me.com>
2022-05-13 18:17:39 +0200

    Liskov Substitution
    Details:
    ---
    * before refactor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import "./style.css";
-import "./clean-code/07-tarea";
-import "./solid/01-srp";
-import "./solid/02-open-close-a";
+import './style.css'
+// import "./clean-code/07-tarea";
+// import "./solid/01-srp";
+// import "./solid/02-open-close-a";
+import './solid/03-liskov-a'
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`;
+`
