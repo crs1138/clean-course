b0ec9f983fb228c2ef3ce19ba9dc8944f1725d95
Jan Pozivil <crs1138@me.com>
2022-05-13 18:40:31 +0200

    Liskov Substitution
    Details:
    ---
    * after refactor

--- src/solid/03-liskov-b.ts
+++ src/solid/03-liskov-b.ts
@@ -1,31 +1,70 @@
-export class Tesla {
-    constructor(private numberOfSeats: number) {}
+export abstract class Vehicle {
+    // getNumberOfSeats(): number {
+    //     throw Error('Method not implemented')
+    // }
+    abstract getNumberOfSeats(): number
+}
+
+export class Tesla extends Vehicle {
+    constructor(private numberOfSeats: number) {
+        super()
+    }
 
     getNumberOfTeslaSeats() {
         return this.numberOfSeats
     }
-}
 
-export class Audi {
-    constructor(private numberOfSeats: number) {}
-
-    getNumberOfAudiSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats
     }
 }
 
-export class Toyota {
-    constructor(private numberOfSeats: number) {}
+export class Audi extends Vehicle {
+    constructor(private numberOfSeats: number) {
+        super()
+    }
 
-    getNumberOfToyotaSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats
     }
 }
 
-export class Honda {
-    constructor(private numberOfSeats: number) {}
+export class Toyota extends Vehicle {
+    constructor(private numberOfSeats: number) {
+        super()
+    }
 
-    getNumberOfHondaSeats() {
+    getNumberOfSeats() {
+        return this.numberOfSeats
+    }
+}
+
+export class Honda extends Vehicle {
+    constructor(private numberOfSeats: number) {
+        super()
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats
+    }
+}
+
+export class Volvo extends Vehicle {
+    constructor(private numberOfSeats: number) {
+        super()
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats
+    }
+}
+
+export class Ford extends Vehicle {
+    constructor(private numberOfSeats: number) {
+        super()
+    }
+
+    getNumberOfSeats() {
         return this.numberOfSeats
     }
 }
