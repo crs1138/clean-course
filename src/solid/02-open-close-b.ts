b3477674fb0bd2ed390f9b3aa5fdaecb9b0dc140
Jan Honza Pozivil <jpozivil@leadtech.com>
2022-05-13 08:16:13 +0200

    02-open-close after refactor

--- src/solid/02-open-close-b.ts
+++ src/solid/02-open-close-b.ts
@@ -1,9 +1,8 @@
-// Hay que agregar la dependencia de axios ```yarn add axios```
-import axios from "axios";
-
+import { HttpClient } from "./02-open-close-c";
 export class TodoService {
+  constructor(private http: HttpClient) {}
   async getTodoItems() {
-    const { data } = await axios.get(
+    const { data } = await this.http.get(
       "https://jsonplaceholder.typicode.com/todos/"
     );
     return data;
@@ -11,8 +10,9 @@ export class TodoService {
 }
 
 export class PostService {
+  constructor(private http: HttpClient) {}
   async getPosts() {
-    const { data } = await axios.get(
+    const { data } = await this.http.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
     return data;
@@ -20,8 +20,9 @@ export class PostService {
 }
 
 export class PhotosService {
+  constructor(private http: HttpClient) {}
   async getPhotos() {
-    const { data } = await axios.get(
+    const { data } = await this.http.get(
       "https://jsonplaceholder.typicode.com/photos"
     );
     return data;
